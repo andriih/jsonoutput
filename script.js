9f26f352cc871289100a63d4e7f3887b4064fae3
andriih <andrii.hnatyshyn@gmail.com>
2017-01-29 23:43:22 +0200

    done

--- script.js
+++ script.js
@@ -8,34 +8,39 @@ xhr.addEventListener("readystatechange",function(){
      }
      var str = JSON.parse(xhr.responseText);
 
-    var newTr = document.createElement("tr");
-
-    var userNameField = document.createElement("td");
-    var birthDayField = document.createElement("td");
-    var professionField = document.createElement("td");
-    var addressField = document.createElement("td");
-
     for(var i = 0;i<=str.length-1;i++){
+        var newTr = document.createElement("tr"),
+            userNameField = document.createElement("td"),
+            birthDayField = document.createElement("td"),
+            professionField = document.createElement("td"),
+            addressField = document.createElement("td"),
+            removeField = document.createElement("td");
+
+
         userNameField.innerText = str[i].userName;
         birthDayField.innerText = str[i].birthDay;
         professionField.innerText =  str[i].profession;
         addressField.innerText = str[i].address;
+        removeField.innerHTML = '<button type="click" class="btn" name="btn">Remove</button>';
 
         newTr.appendChild(userNameField);
         newTr.appendChild(birthDayField);
         newTr.appendChild(professionField);
         newTr.appendChild(addressField);
+        newTr.appendChild(removeField);
 
         table.appendChild(newTr);
-
     };
 
 
+    table.addEventListener("click",function(e){
+        var target = e.target;
 
-
-
-
-
+        if(target.innerText === "Remove"){
+            target.parentElement.parentElement.remove();
+            console.log(str.length);
+        }
+    });
 });
 
 xhr.send();
